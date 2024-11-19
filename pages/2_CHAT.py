d7096620aa155671291b685ed80ff2b65785feeb
Choiseyoon00 <uneeun14@gmail.com>
2024-11-19 15:04:05 +0900

    Update 2_CHAT.py

--- pages/2_CHAT.py
+++ pages/2_CHAT.py
@@ -4,10 +4,33 @@ import numpy as np
 from streamlit_chat import message
 
 
+assistant = client.beta.assistants.create(
+    instructions = "you are a chatbot",
+    model = "gpt-4-turbo",
+    tools = tools
+)
+thread = client.beta.threads.create(
+  messages=[
+    {
+        "role":"user",
+        # "content": "다음 이차방정식의 해를 구해줘: 15x^2 - 2x+1.2=0"
+    }
+  ]
+)
+
+
 if prompt := st.chat_input("What is up?"):
   st.chat_message("user").markdown(prompt) 
   st.session_state.messages.append({"role": "user", "content": prompt}) 
-  response = f"Echo: {prompt}"
+
+
+ response = f"Echo: {prompt}"
+
+ with st.chat_message("assistant"):
+ st.markdown(response)
+
+ st.session_state.messages.append({"role": "assistant", "content": response})
+
 
 
 
