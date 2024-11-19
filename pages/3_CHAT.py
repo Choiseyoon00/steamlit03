a3c40380f6fd80bdcfd23a2142e34341620d769b
Choiseyoon00 <uneeun14@gmail.com>
2024-11-19 15:28:23 +0900

    Update 3_CHAT.py

--- pages/3_CHAT.py
+++ pages/3_CHAT.py
@@ -28,41 +28,6 @@ if prompt:
 
 
 
-while True:
-  # 사용자 입력
-  user_msg = input("user: ")
-  if user_msg == 'STOP':
-    break
-
-  new_message = client.beta.threads.messages.create(
-    thread_id = thread.id,
-    role="user",
-    content=user_msg
-  )
-
-  run = client.beta.threads.runs.create_and_poll(
-    thread_id=thread.id,
-    assistant_id=assistant.id,
-    poll_interval_ms=2000
-  )
-  if run.status in ['expired','failed','cancelled','incomplete']:
-    print("Error:", run.status)
-    break
-
-  thread_messages = client.beta.threads.messages.list(thread.id, limit=1)
-  for msg in thread_messages.data:
-    print(f"{msg.role}: {msg.content[0].text.value}")
-
-
-
-if prompt := st.chat_input("What is up?"):
-  st.chat_message("user").markdown(prompt) 
-  st.session_state.messages.append({"role": "user", "content": prompt}) 
-
-with st.chat_message("assistant"):
-    st.markdown(response)
-    st.session_state.messages.append({"role": "assistant", "content": response})
-
 
 
 
