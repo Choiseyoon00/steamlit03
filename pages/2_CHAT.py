11d2cb78ddeb0dbbf4aace75154a31ea06457054
Choiseyoon00 <uneeun14@gmail.com>
2024-11-19 15:00:33 +0900

    Update 2_CHAT.py

--- pages/2_CHAT.py
+++ pages/2_CHAT.py
@@ -8,9 +8,7 @@ if prompt := st.chat_input("What is up?"):
   st.chat_message("user").markdown(prompt) 
   st.session_state.messages.append({"role": "user", "content": prompt}) 
   response = f"Echo: {prompt}"
-  with st.chat_message("assistant"):
-    st.markdown(response)
-  st.session_state.messages.append({"role": "assistant", "content": response})
+
 
 
 
