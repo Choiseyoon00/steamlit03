36efffee3544ac73dc1725313acaac34c039dcf3
Choiseyoon00 <uneeun14@gmail.com>
2024-11-19 14:57:30 +0900

    Update 2_CHAT.py

--- pages/2_CHAT.py
+++ pages/2_CHAT.py
@@ -7,3 +7,18 @@ from streamlit_chat import message
 if prompt := st.chat_input("What is up?"):
   st.chat_message("user").markdown(prompt) 
   st.session_state.messages.append({"role": "user", "content": prompt}) 
+  response = f"Echo: {prompt}"
+  with st.chat_message("assistant"):
+  st.markdown(response)
+  st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+
+
+
+if "messages" not in st.session_state: ##메모리 초기화
+ st.session_state.messages = [] 
+
+for msg in st.session_state.messages: ##메세지 구분
+ with st.chat_message(msg["role"]):
+ st.markdown(msg["content"])
