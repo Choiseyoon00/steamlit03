d617de11ce936b3a61e5fbb0e71cc1fd99e30b1b
Choiseyoon00 <uneeun14@gmail.com>
2024-11-20 15:18:57 +0900

    sss

--- pages/4_PKNULIBrules.py
+++ pages/4_PKNULIBrules.py
@@ -473,7 +473,7 @@ if prompt := st.chat_input("도서관 규정에 대한 질문을 입력하세요
 
     # OpenAI API 호출
     try:
-        response = client.chat.completion.create(
+        response = client.chat.completions.create(
             model="gpt-4",
             messages=st.session_state.messages
         )
