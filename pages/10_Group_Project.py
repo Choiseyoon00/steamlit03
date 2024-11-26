357e156d7ec0a0e667745c2e8a4549ea680eff1c
Choiseyoon00 <uneeun14@gmail.com>
2024-11-26 16:34:46 +0900

    Update 10_Group_Project.py

--- pages/10_Group_Project.py
+++ pages/10_Group_Project.py
@@ -24,8 +24,32 @@ folium.Marker(
 
 st_data = st_folium(m, width=725)
 
+m.add_child(folium.LatLngPopup()) 
 
-#부경대 영역표시 좌표 검색하기
+
+
+#부경대 영역표시 Dynamic layer control
+var map_div = L.map(
+    "map_div",
+    {
+        center: [north, east],
+        crs: L.CRS.EPSG3857,
+        zoom: 17,
+        zoomControl: true,
+        preferCanvas: false,
+    }
+);
+
+var tile_layer_div_0 = L.tileLayer(
+    "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {"attribution": "\u0026copy; \u003ca href=\"https://www.openstreetmap.org/copyright\"\u003eOpenStreetMap\u003c/a\u003e contributors", "detectRetina": false, "maxNativeZoom": 19, "maxZoom": 21, "minZoom": 0, "noWrap": false, "opacity": 1, "subdomains": "abc", "tms": false}
+);
+
+
+tile_layer_div_0.addTo(map_div);
+
+var drawnItems = [];
+
 var feature_group_feature_group_0 = L.featureGroup(
         {}
     );
@@ -52,7 +76,7 @@ function geo_json_feature_group_0_0_add (data) {
     geo_json_feature_group_0_0
         .addData(data);
 }
-    geo_json_feature_group_0_0_add({"bbox": [-122.397759, 37.794712, -122.396171, 37.795695], "features": [{"bbox": [-122.397759, 37.794712, -122.396171, 37.795695], "geometry": {"coordinates": [[[-122.397416, 37.795017], [-122.397137, 37.794712], [-122.396332, 37.794983], [-122.396171, 37.795483], [-122.396858, 37.795695], [-122.397652, 37.795466], [-122.397759, 37.79511], [-122.397416, 37.795017]]], "type": "Polygon"}, "id": "0", "properties": {}, "type": "Feature"}], "type": "FeatureCollection"});
+    geo_json_feature_group_0_0_add({"bbox": [-122.397759, 37.794712, -122.396171, 37.795695], "features": [{"bbox": [-122.397759, 37.794712, -122.396171, 37.795695], "geometry": {"coordinates": [[[35.13537883403971, 129.10089437313093], [35.13601158899595, 129.10856220310063], [35.135002276392, 129.11011582641603], [35.12992811181822, 129.10562188009783], [35.131749730479164, 129.1013962047231 ]]], "type": "Polygon"}, "id": "0", "properties": {}, "type": "Feature"}], "type": "FeatureCollection"});
 
 
 
