76066c95fcdedc6c7c66d5a087ce807ebafecc10
Choiseyoon00 <uneeun14@gmail.com>
2024-11-26 16:57:43 +0900

    1

--- pages/10_Group_Project.py
+++ pages/10_Group_Project.py
@@ -39,11 +39,10 @@ folium.Marker(
 
 st_data = st_folium(m, width=725)
 
-
 #점선 추가
 folium.PolyLine(
     locations=pknu_boundary_coords,  # 경계선 좌표
     color="green",  # 선 색깔
-    weight=5,      # 선 두께
+    weight=10,      # 선 두께
     dash_array='5, 5'  # 점선 설정 (숫자는 대시 길이와 간격)
 ).add_to(m)
