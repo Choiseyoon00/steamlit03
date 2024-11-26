f423af5a7a5eab66f8dcbc94798b534fd3b47884
Choiseyoon00 <uneeun14@gmail.com>
2024-11-26 16:44:46 +0900

    123

--- pages/10_Group_Project.py
+++ pages/10_Group_Project.py
@@ -6,18 +6,22 @@ from streamlit_folium import st_folium
 
 
 #부경대 좌표와 지도에서 표시
-north = 35.134080249513474
-east = 129.10317348438963
-location_name = "Pukyong National University - Daeyeon Campus"
+pknu_latitude = 35.134080249513474
+pknu_longitude = 129.10317348438963
+pknu = "Pukyong National University - Daeyeon Campus"
 
 # center on PKNU, add marker
 m = folium.Map(
-    location=[north, east],
+    location=[latitude, longitude],
     zoom_start=15
 )
+
+#클릭하면 좌표 표시
+m.add_child(folium.LatLngPopup()) 
+
 folium.Marker(
-  location=[north, east],
-  popup=location_name,
+  location=[latitude, longitude],
+  popup=pknu,
   icon=folium.Icon(color='red',icon='star')
 ).add_to(m)
 
