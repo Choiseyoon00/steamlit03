ea754a4cf6eb65b6d39e7246b93e76bab377fff7
Choiseyoon00 <uneeun14@gmail.com>
2024-11-26 23:43:49 +0900

    Update tools.py

--- lib/tools.py
+++ lib/tools.py
@@ -30,3 +30,26 @@ SCHEMA_GENERATE_IMAGE = {
         "strict": True
     }
 }
+
+UPDATE_MAP = {
+  "name": "update_map",
+  "description": "Update map to center on a particular location",
+  "parameters": {
+    "type": "object",
+    "properties": {
+      "longitude": {
+        "type": "number",
+        "description": "Longitude of the location to center the map on"
+      },
+      "latitude": {
+        "type": "number",
+        "description": "Latitude of the location to center the map on"
+      },
+      "zoom": {
+        "type": "integer",
+        "description": "Zoom level of the map"
+      }
+    },
+    "required": ["longitude", "latitude", "zoom"]
+  }
+}
