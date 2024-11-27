cbcfc4e1390a0b26e90e5b78b72c28c4e340ef39
Choiseyoon00 <uneeun14@gmail.com>
2024-11-27 19:12:27 +0900

    1

--- pages/11_test.py
+++ pages/11_test.py
@@ -5,6 +5,9 @@ import streamlit as st
 
 key="AIzaSyAYbxJm_JxPYaoxw0c-bsP1hDONYcFnQrw"
 
+GOOGLE_API_KEY = key
+
+
 def get_session_url(api_key):
     create_session_url = "https://tile.googleapis.com/v1/createSession"
 
